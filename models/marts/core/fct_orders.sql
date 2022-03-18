6d69471caf22c73445c8c8aeee9b662ff573b201
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-18 11:50:52 +0000

    Dim Customers refactoring

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -8,9 +8,10 @@ final as (
     select
          orders.order_id
         ,orders.customer_id
+        ,orders.order_date
         ,coalesce(sum(case when payments.status='success' then amount end),0) as amount
     from orders
     left join payments using (order_id)
-    group by 1, 2
+    group by 1, 2, 3
 )
 select * from final
