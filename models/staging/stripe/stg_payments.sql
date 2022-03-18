a62285714670aa281266356317a6640fed80f78d
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-18 14:05:59 +0000

    Practice

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -6,6 +6,6 @@ with payments as (
         ,status
         ,amount/100    as amount
         ,created       as payment_date
-    from raw.stripe.payment
+    from {{ source('stripe','payments') }}
 )
 select * from payments
