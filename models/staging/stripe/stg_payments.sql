5db4ec6b6ec197d14d309c7cbc91ab8cbc6c66b3
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-22 20:44:51 +0000

    Practice

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,12 @@
 with payments as (
     select
-         id                                 {#           #}as payment_id
-        ,orderid                            {#           #}as order_id
-        ,paymentmethod                      {#          -#}as payment_method
-        ,status                             {#          -#}as status
-        ,{{ cents_to_dollars('amount',4) }}                as amount
-        ,created                            {#           #}as payment_date
+         id                                 {#           #} as payment_id
+        ,orderid                            {#           #} as order_id
+        ,paymentmethod                      {#           #} as payment_method
+        ,status                             {#           #} as status
+        ,{{ cents_to_dollars('amount',4) }}                 as amount
+        ,created                            {#           #} as payment_date
     from {{ source('stripe','payment') }}
 )
 select * from payments
+{{ limit_data_in_dev('payment_date',1500) }}
