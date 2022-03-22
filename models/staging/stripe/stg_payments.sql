7248c528128dca7ed962d666f9fcef5577084b17
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-22 15:11:23 +0000

    Cents_to_dollars macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,11 @@
 with payments as (
     select
-         id            as payment_id
-        ,orderid       as order_id
-        ,paymentmethod as payment_method
-        ,status
-        ,amount/100    as amount
-        ,created       as payment_date
+         id                                 {#           #}as payment_id
+        ,orderid                            {#           #}as order_id
+        ,paymentmethod                      {#          -#}as payment_method
+        ,status                             {#          -#}as status
+        ,{{ cents_to_dollars('amount',4) }}                as amount
+        ,created                            {#           #}as payment_date
     from {{ source('stripe','payment') }}
 )
 select * from payments
