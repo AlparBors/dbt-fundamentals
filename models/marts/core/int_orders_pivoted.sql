15cbe85230d1bbf57f47bef6b489a5cde0e9ccdc
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-22 14:24:38 +0000

    Practice

--- models/marts/core/int_orders_pivoted.sql
+++ models/marts/core/int_orders_pivoted.sql
@@ -5,16 +5,12 @@ with payments as (
 ),
 final as (
    select
-       order_id,
-       {% for payment_method in payment_methods -%}
-       
-       sum(case when payment_method='{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount 
-       
-       {%- if not loop.last -%}
-       ,
-       {% endif -%}
-       
-       {%- endfor %}
+       order_id
+      {% for payment_method in payment_methods -%}
+      {# -#}
+      ,sum(case when payment_method='{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount 
+      {# -#}
+      {%- endfor -%}
    from payments
    group by 1
 )
