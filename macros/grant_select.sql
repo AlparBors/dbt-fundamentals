0ea4d82b935a6fc652ba4be59ddee92e5c206acf
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-24 17:33:07 +0000

    Grant_select fix

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,13 +1,14 @@
-{% macro grant_select(schema=target.schema, role=target.role) %}
+{% macro grant_select(schema=target.schema, role='test') %}
 
   {% set sql %}
-  grant usage on schema {{ schema }} to role {{ role }};
-  grant select on all tables in schema {{ schema }} to role {{ role }};
-  grant select on all views in schema {{ schema }} to role {{ role }};
+    grant usage on database {{ target.database }} to role {{ role }};
+    grant usage on schema {{ schema }} to role {{ role }};
+    grant select on all tables in schema {{ schema }} to role {{ role }};
+    grant select on all views in schema {{ schema }} to role {{ role }};
   {% endset %}
 
-  {{ log('Granting select on all tables and views in schema ' ~ target.schema ~ ' to role ' ~ role, info=True) }}
-  {% do dbt_utils.run_query(sql) %}
+  {{ log('Granting select on all tables and views in schema ' ~ schema ~ ' to role ' ~ role, info=True) }}
+  {% do run_query(sql) %}
   {{ log('Privileges granted', info=True) }}
 
 {% endmacro %}
