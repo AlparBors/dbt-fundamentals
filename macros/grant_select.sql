0b6a3a503eb646b910aa225ca341d469e4ae910b
Alpár Bors <alpar.bors@hiflylabs.com>
2022-03-24 17:44:00 +0000

    Grant_select log upgrade

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,14 +1,15 @@
-{% macro grant_select(schema=target.schema, role='test') %}
+{% macro grant_select(schema=target.schema, role='test') -%}
 
-  {% set sql %}
+  {%- set sql %}
     grant usage on database {{ target.database }} to role {{ role }};
     grant usage on schema {{ schema }} to role {{ role }};
     grant select on all tables in schema {{ schema }} to role {{ role }};
     grant select on all views in schema {{ schema }} to role {{ role }};
-  {% endset %}
+  {%- endset -%}
 
   {{ log('Granting select on all tables and views in schema ' ~ schema ~ ' to role ' ~ role, info=True) }}
-  {% do run_query(sql) %}
+  {{ log('Running query: ' ~ sql, info=True) }}
+  {%- do run_query(sql) -%}
   {{ log('Privileges granted', info=True) }}
 
-{% endmacro %}
+{%- endmacro %}
